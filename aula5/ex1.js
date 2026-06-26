701d539910842ce51a4c92963a207b72c37c58f2
emanueli <emanueli.dalmolin@escola.pr.gov.br>
2026-06-26 08:20:31 -0300

    corrreção de exercicio

--- aula5/ex1.js
+++ aula5/ex1.js
@@ -1,1 +1,15 @@
-const prompt = require("prompt-sync");
+const express = require('express');
+const app = express();
+
+app.get('/sobre', (req, res) => {
+  res.json({
+    nome: 'Emanueli',
+    disciplina: 'Backend',
+    ano: 2024
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
